532d32808779e8720c8148b3ce64ad71001e7f94
Pascal Poizat <pascal.poizat@lip6.fr>
2016-01-07 16:02:08 +0100

    initial commit

---
+++ src/main/java/Point.java
@@ -0,0 +1,49 @@
+/**
+ * template-java-project
+ * Copyright (C) 2016 pascalpoizat (@pascalpoizat)
+ * <p>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+public class Point {
+
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public int getX() {
+        return x;
+    }
+
+    public void setX(int x) {
+        this.x = x;
+    }
+
+    public int getY() {
+        return y;
+    }
+
+    public void setY(int y) {
+        this.y = y;
+    }
+
+    @Override
+    public String toString() {
+        return String.format("(%d, %d)", getX(), getY());
+    }
+}
---
+++ src/test/java/PointTest.java
@@ -0,0 +1,47 @@
+import static org.testng.Assert.*;
+
+/**
+ * template-java-project
+ * Copyright (C) 2016 pascalpoizat (@pascalpoizat)
+ * <p>
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ * <p>
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ * <p>
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+public class PointTest {
+
+    @org.testng.annotations.Test
+    public void testGetX() throws Exception {
+
+    }
+
+    @org.testng.annotations.Test
+    public void testSetX() throws Exception {
+
+    }
+
+    @org.testng.annotations.Test
+    public void testGetY() throws Exception {
+
+    }
+
+    @org.testng.annotations.Test
+    public void testSetY() throws Exception {
+
+    }
+
+    @org.testng.annotations.Test
+    public void testToString() throws Exception {
+
+    }
+}
