1a75c8af61e0ee086adcf42e51a996d88b9e394a
pascalpoizat <pascal.poizat@lip6.fr>
2019-01-15 11:52:55 +0100

    refactor: clean code

--- src/main/java/fr/paris10/pascalpoizat/templates/javaproject/Point.java
+++ src/main/java/fr/paris10/pascalpoizat/templates/javaproject/Point.java
@@ -27,21 +27,13 @@ public class Point {
         this.y = y;
     }
 
-    public int getX() {
-        return x;
-    }
+    public int getX() {return x;}
 
-    public void setX(int x) {
-        this.x = x;
-    }
+    public void setX(int x) {this.x = x;}
 
-    public int getY() {
-        return y;
-    }
+    public int getY() {return y;}
 
-    public void setY(int y) {
-        this.y = y;
-    }
+    public void setY(int y) {this.y = y;}
 
     @Override
     public String toString() {
